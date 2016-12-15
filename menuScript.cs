b8d181085426fc01f5bb0689464a1f8331d042c0
KylieRocks <activistkylie@gmail.com>
2016-12-15 15:55:37 -0600

    Adding Menu Script

---
+++ menuScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button startText;
+	public Button continueText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		continueText = continueText.GetComponent<Button> ();
+
+		quitMenu.enabled = false;
+	}
+	
+	public void ExitPress() {
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+		continueText.enabled = false;
+	}
+
+	public void NoExitPress() {
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		continueText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartLevel() {
+		Application.LoadLevel (1);
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+}
